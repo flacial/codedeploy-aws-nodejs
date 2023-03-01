42cf7233e285b967b8bcff0880472842278d1418
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-03-01 10:17:20 +0400

    Apply modifications

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-forever stopall
-forever start /home/ubuntu/nodejs/app.js
+# forever stopall
+# forever start /home/ubuntu/nodejs/app.js
+
+# Change to the app directory
+cd /home/ec2-user/app
+
+# Start the server
+sudo node app.js
